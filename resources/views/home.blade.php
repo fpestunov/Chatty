e5f290fc6a221dfcb2e93d6baebf335d823cd15e
fpestunov <fpestunov@gmail.com>
2018-10-18 20:49:37 +0400

    Add Home controller and base template

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.default')
+
+@section('title', 'Home Page')
+
+@section('content')
+    <h1>Welcome to Chatty</h1>
+    <p>New social network...</p>
+@endsection
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Chatty</a>
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  <div class="collapse navbar-collapse">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
