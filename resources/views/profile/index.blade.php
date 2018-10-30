c41633dd48cadd61d44dcab38f1f600cfa46b952
fpestunov <fpestunov@gmail.com>
2018-10-30 16:32:15 +0400

    Add Friend model methods

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -9,6 +9,15 @@
             @include('user.userblock')
         </div>
         <div class="col-lg-4 col-lg-offset-3">
+            @if (Auth::user()->hasFriendRequestsPending($user))
+                <p>Waiting for {{ $user->getNameOrUsername() }} to accept your request.</p>
+            @elseif (Auth::user()->hasFriendRequestsReceived($user))
+                <a href="#" class="btn btn-primary">Accept friend request</a>
+            @elseif (Auth::user()->isFriendsWith($user))
+                <p>You and {{ $user->getNameOrUsername() }} are friends.</p>
+            @else
+                <a href="#" class="btn btn-primary">Add as friend</a>
+            @endif
             <h4>{{ $user->getFirstNameOrUsername() }}'s friends.</h4>
             @if (!$user->friends()->count())
                 <p>{{ $user->getFirstNameOrUsername() }} has no friends.</p>
